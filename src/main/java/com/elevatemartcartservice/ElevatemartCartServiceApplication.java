15aae8b60c2fee003fd225e9cc0b13dd2679869e
shoaib-ahmed16 <shoaib-ahmed16>
2024-02-03 15:48:39 +0530

    initializing the cart service repository

---
+++ src/main/java/com/elevatemartcartservice/ElevatemartCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.elevatemartcartservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatemartCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatemartCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elevatemartcartservice/ElevatemartCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevatemartcartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatemartCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
