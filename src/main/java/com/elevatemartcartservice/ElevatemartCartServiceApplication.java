94b81f2c02cda1c4b6cea0ff551456d4fbc28e3b
shoaib-ahmed16 <shoaib-ahmed16>
2024-03-23 15:21:46 +0530

    Adding the chagnes

--- src/main/java/com/elevatemartcartservice/ElevatemartCartServiceApplication.java
+++ src/main/java/com/elevatemartcartservice/ElevatemartCartServiceApplication.java
@@ -3,10 +3,14 @@ package com.elevatemartcartservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Profile;
 
 @EnableFeignClients
+@EnableConfigServer
 @SpringBootApplication
+@Profile("local")
 @EntityScan(basePackages = "com.elevatemartcartservice.domain")
 public class ElevatemartCartServiceApplication {
 
